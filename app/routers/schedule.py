a2445765c55eae4c71204377c0798355e83aeb0e
mz <118102747+mzdevI@users.noreply.github.com>
2023-04-09 18:13:26 +0400

    Update schedule.py

--- app/routers/schedule.py
+++ app/routers/schedule.py
@@ -1,29 +1,27 @@
-from typing import Optional, List
+from typing import Optional, List, Union
 from fastapi import FastAPI, Response, HTTPException, status, Form, Depends, Body, APIRouter
 from pydantic import BaseModel, EmailStr
-from database import *
-from sqlalchemy.orm import Session6
+from database import Lesson, get_db
+from sqlalchemy.orm import Session
 from datetime import datetime
+from database import *
 import oauth2
 
 router = APIRouter(
     tags=["Lesson"]
 )
 
-"""
-Schedule fetching to the frontend
 
-The process:
-- Get the current user
-- Get the current user's class id
-- All the lessons with the class id
-- Sort them by the day and lesson number, for example the first returned lesson will be the first lesson of the first day and so on
-- Return the lessons 
-
-The frontend will get all the lessons and then will display them through a nested loop, first the days and then the lessons
-Then get the lessons name and color and display them
-
-If the the user is on a mobile page, the frotend will have to make a quiry to the backend to get the lessons of the current day
-"""
-
-
+@router.get("/api/lessons")
+def get_lessons(db: Session = Depends(get_db), current_user: str = Depends(oauth2.get_current_user), day: Optional[str] = None):
+    
+    user: Union[Student, Teacher, Parent, Admin] = db.query(Student).filter(Student.id == current_user.id).first()
+    
+    
+    if day:
+        lessons = db.query(Lesson).filter(Lesson.class_id == user.class_id, Lesson.day == day).all()
+    if not day:
+        lessons = db.query(Lesson).filter(Lesson.class_id == user.class_id).all()
+    
+        
+    return lessons 
